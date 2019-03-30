2a5103ea2f10877de22a23d3cfbec12f1da6aa1a
vlad-bilyk <bilyk.v@ucu.edu.ua>
2019-03-30 16:53:58 +0200

    mesht

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Hi there!"
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
