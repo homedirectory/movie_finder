5d8791dcfc82f24d7ff8028a58bb68f63bb74012
vlad-bilyk <bilyk.v@ucu.edu.ua>
2019-03-30 18:25:49 +0200

    mesht

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_wtf import FlaskForm
-from find_movie import *
+# from find_movie import *
 from get_poster import *
+from find_movie_2 import *
 
 
 app = Flask(__name__)
@@ -13,16 +14,19 @@ def homepage():
         try:
             year = int(request.form['year'])
             rating = request.form['radio-group']
-            genres = request.form.getlist('genre')
+            print(rating)
             rating = int(rating[0]) if "+" in rating else -1 * int(rating[0])
-            print(year, rating, genres)
-            movie = find_movie(year, rating, genres)[0]
-            poster = get_poster(movie)
-            print(movie)
+            genres = request.form.getlist('genre')
+            popularity = request.form['pop']
+            print(year, rating, genres, popularity)
+            movie_name = find_movie(year, rating, popularity, genres)[0]
+            poster = get_poster(movie_name)
+            print(movie_name)
             print(poster)
+            # return render_template('result.html', movie=movie_name, poster=poster)
         except NoSuchMovie as e:
             print(e)
-            print("Mesht")
+            print("Mesht no movies")
             error = "Something went wrong. Please try again"
         #     # flash(error)
             return render_template('homepage.html', error=error)
@@ -30,6 +34,9 @@ def homepage():
             print(e)
             error = "Something went wrong. Please try again"
             return render_template('homepage.html', error=error)
+        except:
+            return render_template('homepage.html')
+
 
 
 
