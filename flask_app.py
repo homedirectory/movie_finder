1e41c767ac871c1674b02e9cfe6adc6adab471e6
vlad-bilyk <bilyk.v@ucu.edu.ua>
2019-03-30 17:32:10 +0200

    mesht

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_wtf import FlaskForm
 
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
+    if request.method == "POST":
+        try:
+            year = request.form['year']
+            rating = request.form['radio-group']
+            genre = request.form.getlist('genre')
+        except:
+            print("Mesht")
+            error = "Something went wrong. Please try again"
+            # flash(error)
+            return render_template('homepage.html', error=error)
+
+
+        print(year, rating, genre)
+        print(type(year), type(rating), type(genre))
+
     return render_template('homepage.html')
 
 
